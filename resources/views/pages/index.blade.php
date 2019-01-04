976796fb645e0cbe6944f4b5ef4b827913eb9ba2
kakaduu <45234828+kakaduu@users.noreply.github.com>
2019-01-04 13:32:44 +0200

    ok

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -29,43 +29,8 @@
             @endif
 
         </div>
-
         <!-- Sidebar -->
-        <div class="col-md-4">
-            <!-- Search -->
-                <div class="card my-4">
-                    <h5 class="card-header">Search</h5>
-                    <div class="card-body">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Search..">
-                            <span class="input-group-btn">
-                                <button class="btn btn-secondary" type="button">Search</button>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-
-            <!-- Categories-->
-            <div class="card my-4">
-                <h4 class="card-header">Categories</h4>
-                <div class="card-body">
-                    <div class="row">
-                            <div class="col-lg-12">
-                                <ul class="list-group list-group-flush">
-                                    @foreach ($categories as $c)
-                                        <li class="list-group-item">
-                                            <a href="{{route('category', ['id' => $c->id])}}" role="button"
-                                                class="bth list-group-item d-flex list-group-item-action justify-content-between alignitems-right">
-                                                {{ $c->name }}
-                                                <span class="badge badge-success">{{$c->posts->count()}}</span></a>
-                                        </li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                    </div>
-                </div>
-            </div>
-        </div><!-- /.sidebar -->
+        @include('layouts.sidebar')
     <!-- /.row -->
 <!-- /.container -->
 @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,8 +16,11 @@
                                 <!-- Avatar -->
                                 <img style="width:10%" src="/storage/avatars/{{$post->user->avatar}}">
                                 <!-- User/Date -->
-                                <small>Post by: {{$post->user->name}}, written on: {{$post->created_at}}, Category: {{$post->category->name}}</small>
+                                <small><i class="fas fa-user-secret"> Post by: {{$post->user->name}}</i>
+                                    <i class="fas fa-hourglass-half">, written on: {{$post->created_at}}</i>
+                                    <i class="fas fa-tag">, Category: {{$post->category->name}}</i> </small>
                                 <hr>
+
                         @endforeach
                         {{$posts->links()}}
                     @else
@@ -26,40 +29,7 @@
                 </div>
 
                 <!-- Sidebar -->
-                <div class="col-md-4">
-                    <!-- Search -->
-                        <div class="card my-4">
-                            <h5 class="card-header">Search</h5>
-                            <div class="card-body">
-                                <div class="input-group">
-                                    <input type="text" class="form-control" placeholder="Search..">
-                                    <span class="input-group-btn">
-                                        <button class="btn btn-secondary" type="button">Search</button>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    <!-- Categories-->
-                    <div class="card my-4">
-                        <h4 class="card-header">Categories</h4>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <ul class="list-group list-group-flush">
-                                        @foreach ($categories as $c)
-                                            <li class="list-group-item">
-                                                <a href="{{route('category', ['id' => $c->id])}}" role="button"
-                                                    class="bth list-group-item d-flex list-group-item-action justify-content-between alignitems-right">
-                                                    {{ $c->name }}
-                                                    <span class="badge badge-success pull-right">{{$c->posts->count()}}</span></a>
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @include('layouts.sidebar')
         <!-- /.row -->
     <!-- /.container -->
 
